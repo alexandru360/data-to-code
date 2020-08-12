66f1cc4b37d76a8516ff9dd9729a15e06df0ac85
Alexandru Badita <alexandru360@gmail.com>
2020-08-12 15:43:59 +0300

    wip connection strings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './z-main/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
 import { WizzardConnectionComponent } from './wizzard-connection/wizzard-connection.component';
