87bf9bf9730ce0de15ef019338774513fa56af75
Alexandru Badita <alexandru360@gmail.com>
2020-09-07 16:13:57 +0300

    super refactoring

---
+++ projects/data-to-code-present-ui/src/app/header/header-menu/header-menu/header-menu.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-header-menu',
+  templateUrl: './header-menu.component.html',
+  styleUrls: ['./header-menu.component.css']
+})
+export class HeaderMenuComponent implements OnInit {
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  goHome() { this.router.navigate(['/home']); }
+  goProduct() { this.router.navigate(['/home']); }
+  goEmpty() { this.router.navigate(['/home']); }
+}
