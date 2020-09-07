707505a3fb82feb897a8fe3db78f5223c2c5c996
Alexandru Badita <alex.badita@medicasoftllc.com>
2020-09-07 20:16:42 +0300

    fix ?!

---
+++ src/app/body/components/class-and-types-and-tools/constants.ts
@@ -0,0 +1,3 @@
+import {HttpHeaders} from '@angular/common/http';
+
+export const COMMON_HEADER = new HttpHeaders().set('Content-Type', 'application/json');
---
+++ src/app/z-main/services/app-config.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {map} from 'rxjs/operators';
+import {Configuration} from './configuration';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class AppConfigService {
+  private config: Configuration;
+
+  constructor(private http: HttpClient) {}
+
+  load(url: string) {
+    return new Promise((resolve) => {
+      this.http.get<Configuration>(url).pipe(map(res => res))
+        .subscribe(config => {
+          this.config = config;
+          resolve();
+        });
+    });
+  }
+
+  getConfiguration(): Configuration {
+    return this.config;
+  }
+}
