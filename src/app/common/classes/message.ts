ae28f9c5b568f14e7e1ffcfaa23c39fffd8e0fb8
Alexandru Badita <alexandru360@gmail.com>
2020-10-15 19:36:56 +0300

    cool

--- src/app/common/classes/message.ts
+++
@@ -1,6 +0,0 @@
-export class Message {
-  ClientUniqueId: string;
-  Type: string;
-  Msg: string;
-  Date: Date;
-}
--- src/app/product/class-and-types-and-tools/constants.ts
+++
@@ -1,3 +0,0 @@
-import {HttpHeaders} from '@angular/common/http';
-
-export const COMMON_HEADER = new HttpHeaders().set('Content-Type', 'application/json');
