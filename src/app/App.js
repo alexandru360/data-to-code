c7234e3c6ddc28f3eafc040dd66d403ae1006160
Alexandru Badita <alexandru360@gmail.com>
2020-10-18 12:13:39 +0300

    templates in the making ...

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Menu from '../navigation/demo-navigation-menu';
+
+function App() {
+    return (
+        <Menu />
+    );
+}
+
+export default App;
