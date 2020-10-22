cffd183f90eed9a41bcbc2320c2e69fd8d1bab66
Alexandru Badita <alexandru.badita@infoworld.ro>
2020-10-22 12:39:51 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
     document.querySelector('#root')
 );
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
