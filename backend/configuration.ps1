8fd028ce8f5b366a6f338e0ca1cca264effe1b24
ignatandrei <ignatandrei@yahoo.com>
2020-12-01 21:50:05 +0200

    writing version

--- backend/configuration.ps1
+++ backend/configuration.ps1
@@ -1,8 +1,12 @@
-﻿$version = Get-Date -Format "yyyy.M.d.HHmm"
+﻿$date =Get-Date
+$version = $date.ToString("yyyy.M.d.HHmm")
 
+$versionClickOnce=  $date.ToString("yyyy.1MM.1dd.1HHmm")
+
+$revision = $date.ToString("1HHmm")
 
 $files = gci -Path $PSScriptRoot  -Filter "*.csproj" -Recurse 
-$files.length
+
 
 $files | ForEach-Object -Process {
     
@@ -12,6 +16,27 @@ $files | ForEach-Object -Process {
     $xmlFile = [xml](Get-Content -Path $_.FullName)
     $versionXML = $xmlFile.SelectSingleNode("//Version")
     $versionXML.InnerText = $version
+    $xmlFile.Save($_.FullName)
+ 
+ }
+ # writing click once
+$files = gci -Path $PSScriptRoot  -Filter "*.pubxml" -Recurse 
+$files | ForEach-Object -Process {
+    
+    
+    if($_.FullName.Contains("wwwroot")){        return }
+    
+    Write-Host $_.FullName
+
+    $xmlFile = [xml](Get-Content -Path $_.FullName)
+    
+    
+    $versionXML = $xmlFile.SelectSingleNode("//*[local-name()='ApplicationRevision']")
+    $versionXML.InnerText = $revision
+
+    $versionXML = $xmlFile.SelectSingleNode("//*[local-name()='ApplicationVersion']")
+    $versionXML.InnerText = $versionClickOnce
+
     $xmlFile.Save($_.FullName)
  
  }
