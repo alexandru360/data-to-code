c532b38751a16e6b459d6db1cfac52f4301fcf9c
ignatandrei <ignatandrei@yahoo.com>
2020-12-05 07:25:44 +0200

    net core 5 tables

---
+++ backend/GenerateFromDB/wwwroot/GenerateAll/Backend/NETCore5/TestWEBAPI_DAL/Generated/TestFields.cs
@@ -0,0 +1,63 @@
+@model Stankins.Interfaces.IDataToSent
+@{
+    
+	string ClassNameFromTableName(string tableName){
+		return tableName.Replace(" ","").Replace(".","").Replace("(","").Replace(")","");
+	}
+	
+    var ds= Model.FindAfterName("DataSource").Value;
+    var dtOptions= Model.FindAfterName("@@Options@@").Value;
+    
+    var nrRowsDS=ds.Rows.Count;
+    
+    var nameTablesToRender = new string[nrRowsDS];
+    var tables=new System.Data.DataTable[nrRowsDS];
+    for (int iRowDS = 0; iRowDS < nrRowsDS; iRowDS++)
+    {
+        var nameTable = ds.Rows[iRowDS]["TableName"].ToString();
+        var renderTable = Model.FindAfterName(nameTable).Value;
+        nameTablesToRender[iRowDS] = nameTable;
+        tables[iRowDS]=renderTable;
+    }
+	string nameProperty(string original, string nameClass){
+		var name = original.ToLower().Replace(" ","").Replace("event","event1").Replace("class","class1").Replace("object","object1").Replace("<","").Replace("/","").Replace(">","").Replace("(","").Replace(")","").ToLower();
+		if(!IsIdentifier(name))
+			name = "generated_"+name;
+		if(nameClass.ToLower() == name)
+            name= "generated_"+name;
+		return name.Trim();
+	}
+	//https://docs.microsoft.com/en-us/dotnet/api/microsoft.codeanalysis.csharp.syntaxfacts?view=roslyn-dotnet
+	bool IsIdentifier(string text)
+	{
+     if (string.IsNullOrEmpty(text))
+        return false;
+     if (!char.IsLetter(text[0]) && text[0] != '_')
+        return false;
+     for (int ix = 1; ix < text.Length; ++ix)
+        if (!char.IsLetterOrDigit(text[ix]) && text[ix] != '_')
+           return false;
+     return true;
+	}
+
+}
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestWebAPI_BL;
+
+namespace TestWEBAPI_DAL
+{
+    public enum Tables{
+        None = 0
+        @foreach(var nameTable in nameTablesToRender){
+			string nameClass=ClassNameFromTableName(nameTable);
+            
+            <text>
+            ,@(nameClass)
+            </text>
+        }
+
+    }
+}
