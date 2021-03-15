fb3d9b1b49a24e3f6cfda029cc9b2b0c9a83e6ef
Alexandru Badita <alexandru360@gmail.com>
2021-03-15 10:44:08 +0200

    electronization

---
+++ toolbox/src/download.js
@@ -0,0 +1,28 @@
+﻿'use strict'
+
+const Fs = require('fs')
+const Path = require('path')
+const Axios = require('axios')
+
+async function downloadImage(
+        apiUrl = 'https://www.data-to-code.eu/app/GenerateAppWPF.application'
+    ) {
+    const url = apiUrl
+    const path = Path.resolve(__dirname, 'download', 'GenerateAppWPF.application')
+    const writer = Fs.createWriteStream(path)
+
+    const response = await Axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    })
+
+    response.data.pipe(writer)
+
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve)
+        writer.on('error', reject)
+    })
+}
+
+module.exports = {downloadImage};
